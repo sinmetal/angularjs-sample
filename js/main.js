caa5ba391ac3aa2488cc68ec8f0231abcefd95a5
sinmetal <metal.tie@gmail.com>
2013-05-18 20:04:40 +0900

    Controllerを即時関数の中に隠した。 #3

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-var app = angular.module("sample", ["ngResource"]);
-app.controller("MainController", function($scope, $resource){
-  $scope.categories = [{"id" : "1", "name" : "野菜"}];
-  var List = $resource("/item/list");
-  var Entry = $resource("/store/entry");
-  $scope.changeCategory = function() {
-    $scope.items = List.query({id : $scope.entryForm.categoryid}, function(){
-      console.log("success list");
-    }, function(){
-      console.log("error list");
-    });
-  };
-  $scope.submit = function($event) {
-    Entry.save($scope.entryForm, function(){
-      console.log("success entry");
-    }, function(){
-      console.log("error entry");
-    });
-  };
-});
+(function() {
+  var app = angular.module("sample", ["ngResource"]);
+  app.controller("MainController", function($scope, $resource){
+    $scope.categories = [{"id" : "1", "name" : "野菜"}];
+    var List = $resource("/item/list");
+    var Entry = $resource("/store/entry");
+    $scope.changeCategory = function() {
+      $scope.items = List.query({id : $scope.entryForm.categoryid}, function(){
+        console.log("success list");
+      }, function(){
+        console.log("error list");
+      });
+    };
+    $scope.submit = function($event) {
+      Entry.save($scope.entryForm, function(){
+        console.log("success entry");
+      }, function(){
+        console.log("error entry");
+      });
+    };
+  });
+})();
