d31f25920671dc331b095ca259c3ffbf7b2df269
sinmetal <metal.tie@gmail.com>
2013-05-18 21:24:02 +0900

    appengine操作を追加してビルドが通った #4

--- store/entry.go
+++ store/entry.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"appengine"
+	"appengine/datastore"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,11 +12,29 @@ func init() {
 	http.HandleFunc("/store/entry", handler)
 }
 
+type Test struct {
+	Name string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusNotFound)
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		fmt.Fprintf(w, "Not Found")
+	if r.Method == "POST" {
+		post(w, r)
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "Not Found")
+	return
+
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	key := datastore.NewKey(c, "Test", "", 1, nil)
+	var t Test
+	if _, err := datastore.Put(c, key, &t); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
